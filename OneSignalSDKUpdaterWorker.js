1a3375b88ca3a3f5cbc395eb00e3ec2f8fe4d0da
bullyto <apero.nuit.du.66@gmail.com>
2026-01-17 18:35:41 +0100

    Add files via upload

--- OneSignalSDKUpdaterWorker.js
+++ OneSignalSDKUpdaterWorker.js
@@ -1,2 +1,1 @@
-// OneSignal SDK Updater Service Worker (v16) — wrapper for /maps/ scope
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKUpdaterWorker.js");
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.worker.js");
