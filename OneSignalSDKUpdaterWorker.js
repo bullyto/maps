e6d5d66abee492b763ece0d4ed67e58de48844e9
bullyto <apero.nuit.du.66@gmail.com>
2026-01-18 02:34:27 +0100

    Add files via upload

--- OneSignalSDKUpdaterWorker.js
+++ OneSignalSDKUpdaterWorker.js
@@ -1,1 +1,4 @@
-importScripts('OneSignalSDKWorker.js');
+/* OneSignal Web Push Updater Worker (v16)
+ * Ce fichier doit exister a l'emplacement configure dans OneSignal.
+ */
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,4 @@
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+/* OneSignal Web Push Service Worker (v16)
+ * Scope recommande: /maps/push/
+ */
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
