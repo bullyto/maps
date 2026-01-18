10b4f64b33c1b4b5eaa952440d15a2d57847f6cd
bullyto <apero.nuit.du.66@gmail.com>
2026-01-18 20:51:46 +0100

    Delete push directory

--- push/OneSignalSDKUpdaterWorker.js
+++
@@ -1,4 +0,0 @@
-/* OneSignal Web Push Updater Worker (v16)
- * Ce fichier doit exister a l'emplacement configure dans OneSignal.
- */
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
--- push/OneSignalSDKWorker.js
+++
@@ -1,4 +0,0 @@
-/* OneSignal Web Push Service Worker (v16)
- * Scope recommande: /maps/push/
- */
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
